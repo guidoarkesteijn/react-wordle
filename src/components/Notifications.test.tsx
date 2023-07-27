b5d9637fcd9564c56d3304175277ff08e7ff4703
Guido Arkesteijn <guidoarkesteijn@gmail.com>
2023-07-27 23:04:49 +0200

    Added notifiacations test

--- src/components/Notifications.test.tsx
+++ src/components/Notifications.test.tsx
@@ -1,12 +1,24 @@
 import { test } from "vitest";
-import { NotificationData, NotificationType } from "./Notifications";
+import Notifications, { NotificationData, NotificationType } from "./Notifications";
+import { render } from "@testing-library/react";
+
+let title : string = "Test";
+let type : NotificationType = NotificationType.Normal;
 
 test('NotificationData Constructor', () => {
-    let title : string = "Test";
-    let type : NotificationType = NotificationType.Normal;
 
     let notification : NotificationData = new NotificationData(title, type);
 
     expect(notification.message).toBe(title);
     expect(notification.type).toBe(type);
+});
+
+test('Notifications', () => {
+    const deleteNotification = (index : number) => {};
+    const notifications : NotificationData[] = [];
+    notifications.push(new NotificationData(title, type));
+
+    const wrapper = render(<Notifications notifications={notifications} deleteNotification={deleteNotification}/>)
+
+    expect(wrapper.container.querySelector(".notifications")?.childNodes.length).toBe(1);
 });
