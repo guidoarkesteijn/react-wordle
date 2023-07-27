b6b5e635f2ddf85b8d414005b247187752cb4382
Guido Arkesteijn <guidoarkesteijn@gmail.com>
2023-07-27 23:50:38 +0200

    Added CNAME for deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import './App.css';
 import Board from './components/Board';
 import Header from './components/Header';
