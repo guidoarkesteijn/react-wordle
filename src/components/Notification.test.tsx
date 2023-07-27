a93c749458abca0b6b1f6420a9c48d45a013998d
Guido Arkesteijn <guidoarkesteijn@gmail.com>
2023-07-27 22:51:24 +0200

    Added Notification Test

---
+++ src/components/Notification.test.tsx
@@ -0,0 +1,15 @@
+import { test } from "vitest";
+import { NotificationData, NotificationType } from "./Notifications";
+import { render } from "@testing-library/react";
+import Notification from "./Notification";
+
+test('<Notification>', () => {
+    let deleteNotification = (index : number) => {};
+    const title = "Test";
+    let notification : NotificationData = new NotificationData(title, NotificationType.Normal);
+    const wrapper = render(<Notification key={0} index={0} notification={notification} deleteNotification={deleteNotification}/>)
+    
+    let element = wrapper.container.querySelector("div");
+    expect(element?.className).toBe("notification normal");
+    expect(element?.childNodes[0].textContent).toBe(title);
+});
