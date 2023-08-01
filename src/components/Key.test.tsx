e9d9b438b92864a2f698a5939f81a16762a11553
Guido Arkesteijn <guidoarkesteijn@gmail.com>
2023-08-01 08:49:27 +0200

    Fixed key test

--- src/components/Key.test.tsx
+++ src/components/Key.test.tsx
@@ -2,6 +2,26 @@ import { State } from "../App";
 import Key from "./Key";
 import { render } from '@testing-library/react'
 
-test('<Notification>', () => {
-    const wrapper = render(<Key letter={'l'}state={State.Correct}/>)
+test('Key State Correct', () => {
+    const wrapper = render(<Key letter={'l'} state={State.Correct}/>)
+    const key = wrapper.container.firstElementChild;
+    expect(key?.className).toContain(State.Correct);
+});
+
+test('Key State Location', () => {
+    const wrapper = render(<Key letter={'l'} state={State.Location}/>)
+    const key = wrapper.container.firstElementChild;
+    expect(key?.className).toContain(State.Location);
+});
+
+test('Key State Wrong', () => {
+    const wrapper = render(<Key letter={'l'} state={State.Wrong}/>)
+    const key = wrapper.container.firstElementChild;
+    expect(key?.className).toContain(State.Wrong);
+});
+
+test('Key State Active', () => {
+    const wrapper = render(<Key letter={'l'} state={State.Active}/>)
+    const key = wrapper.container.firstElementChild;
+    expect(key?.className).toContain(State.Active);
 });
